944e35b4d53331ed7b32e451c6c9512f7764e891
ancaza12 <56890947+ancaza12@users.noreply.github.com>
2019-11-28 13:30:27 -0500

    graphs

--- ej32.py
+++ ej32.py
@@ -2,18 +2,23 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-plt.figure()
+t= np.linspace(0,0.1,10)
+plt.figure(figsize=(10,5))
 data = np.loadtxt('ej32.dat')
-plt.plot(data[:,0], data[:,1])
 
-plt.xlabel('X')
-plt.ylabel('Y')
+plt.subplot(131)
+plt.plot(data[:,1],data[:,0])
+plt.xlabel('Posicion')
+plt.ylabel('tiempo')
 
+plt.subplot(132)
+plt.plot(data[:,2], data[:,0])
+plt.xlabel('Posicion')
+plt.ylabel('psi')
 
-##fig = plt.figure()
-##ax1 = fig.add_subplot(111,projection='3d')
-
-##for k in P.keys():
-##    plt.plot(P[k]['x'], P[k]['y'], P[k]['z'], c='black')
+plt.subplot(133)
+plt.plot(data[:,1], data[:,2])
+plt.xlabel('tiempo')
+plt.ylabel('psi(L/4)')
 
 plt.savefig('ej32.png')
